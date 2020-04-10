cd74f9c08d8b50e4b456d11b64b6957c2f868faf
tracy zumbu garcia <tracyzumbu@lilo.org>
2020-04-10 16:38:04 +0200

    added some sccs file and AProposPage JS component

---
+++ src/components/APropos.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export default function APropos() {
+    return (
+        <>
+        <div class="section-image">
+            <div class="text-box--left">
+
+            <div class='heading-primary'>
+                <span class="heading-primary__main--white">NICE TO</span>
+                <span class="heading-primary__main--white">MEET YOU !</span>
+                <ul class="heading-primary__sub--white">
+                    <li>- Notre histoire</li>
+                    <li>- Notre philosophie</li>
+                    <li>- Notre charte</li>
+                </ul>
+                <span class="heading-primary__sub--white"></span>
+                
+            </div>
+            </div>
+        </div>
+
+            
+        </>
+    )
+}
