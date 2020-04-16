fab5402c28b54dfc3281aadd17f2e40e146ddc3d
tracy zumbu garcia <tracyzumbu@lilo.org>
2020-04-16 14:31:44 +0200

    building login content section. designing element. the bottom part is done

---
+++ src/components/ButtonWhite.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+
+const ButtonWhite = (props) => {
+    return (
+        <>
+          <a href="" class="btn btn--white ">{props.text} </a>
+        </>
+    )
+}
+
+export default ButtonWhite;
--- src/screens/ConnexionPage.js
+++ src/screens/ConnexionPage.js
@@ -1,13 +1,14 @@
 import React from "react";
 import HeaderGreenStatic from "../components/HeaderGreenStatic";
 import ButtonBlueBorder from "../components/ButtonBlueBorder";
+import ButtonWhite from "../components/ButtonWhite";
 
 export default function ConnexionPage() {
   return (
     <>
       <div class="login">
         <HeaderGreenStatic />
-       <div class="login__box">
+        <div class="login__box">
           <div class="login__box__image">salut</div>
           <div class="login__box__content">
             <div class="text-box--center">
@@ -19,10 +20,30 @@ export default function ConnexionPage() {
             <div class="login__box__social-media">
               <ButtonBlueBorder text="Continuer avec Facebook" />
               <ButtonBlueBorder text="Continuer avec Google" />
+              <hr></hr>
+              <form action="" method="">
+                <p>
+                  <input type="text" name="email" placeholder="E-mail" />
+                </p>
+                <p>
+                  <input
+                    type="text"
+                    name="password"
+                    placeholder="Mot de passe"
+                  />
+                </p>
+              </form>
+              <a href="#" class="forget_pwd">j'ai oublié mon mot de passe</a>
+              <ButtonWhite text="Se connecter" />
+              <div class="sign-up">
+                <p>vous n'avez pas encore de compte,</p>
+                <a href="#">inscrivez-vous par ici</a>
+                </div>
+
             </div>
             <div class="login__box__input"></div>
           </div>
-          </div>
+        </div>
       </div>
     </>
   );
